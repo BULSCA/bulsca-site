9e5c39ece042612d07d22f3a2e891cba7cfa8942
Noah Hollowell <noahdhollowell@googlemail.com>
2022-05-15 23:01:30 +0100

    weird issue

--- database/migrations/2022_05_09_145109_create_universities_table.php
+++ database/migrations/2022_05_09_145109_create_universities_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('universities', function (Blueprint $table) {
             $table->id();
-            $table->text('name')->unique('unique_uni_name');
+            $table->text('name');
             $table->timestamps();
         });
     }
