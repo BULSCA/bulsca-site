ff44f3515ff57561c87d1560a09e265ea2b0f780
Noah Hollowell <noahdhollowell@googlemail.com>
2022-07-23 21:26:47 +0100

    CHanged header-2 background

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
       <div class="absolute top-0 right-0 w-full h-full head-bg flex items-center justify-center transition-opacity   duration-1000" id="head1">
         <img src="./storage/logo/blogo.png" class="md:w-[12.5%] w-[50%] h-auto" alt="">
       </div>
-      <div class="absolute top-0 right-0 w-full h-full head-bg-2 flex items-center justify-center transition-opacity opacity-0 duration-1000 z-20" id="head2">
+      <div class="absolute top-0 right-0 w-full h-full head-bg-3 flex items-center justify-center transition-opacity opacity-0 duration-1000 z-20" id="head2">
         <img src="./storage/logo/blogo.png" class="md:w-[12.5%] w-[50%] h-auto" alt="">
       </div>
     </div>
