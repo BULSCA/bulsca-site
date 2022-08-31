538349f0f39d5f53cc94720f625af9b5882f5c87
Noah Hollowell <noahdhollowell@googlemail.com>
2022-08-31 10:50:55 +0100

    added champs coord photo

--- resources/views/get-involved/committee.blade.php
+++ resources/views/get-involved/committee.blade.php
@@ -143,7 +143,7 @@ Committee | Get Involved |
 
     <div class="flex flex-col items-center justify-center md:basis-1/5 ">
       <div class="rounded-full w-44 h-44 overflow-hidden flex items-center justify-center">
-        <img src="/storage/logo/blogo.png" class="w-full h-full" alt="">
+        <img src="/storage/photos/committee/champs.jpeg" class="w-full h-full" alt="">
       </div>
       <h3 class="header header-smallish py-4 px-3 border-b border-gray-400">
         Chloe Warr
