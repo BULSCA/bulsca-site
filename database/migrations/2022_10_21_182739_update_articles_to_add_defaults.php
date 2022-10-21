748b96f014378aca1c3461bb5fb93719c4b97569
Noah Hollowell <noahdhollowell@googlemail.com>
2022-10-21 19:30:39 +0100

    fixed default value error on views

---
+++ database/migrations/2022_10_21_182739_update_articles_to_add_defaults.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->integer('views')->default(0)->change();
+            $table->integer('thumbs_up')->default(0)->change();
+            $table->integer('thumbs_down')->default(0)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            //
+        });
+    }
+};
