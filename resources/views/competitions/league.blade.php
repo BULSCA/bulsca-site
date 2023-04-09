9e5c3f93e796b8b9a6cdf10825c8963863b56288
Noah Hollowell <noahdhollowell@googlemail.com>
2023-04-09 16:25:01 +0100

    Added external link image to external results resoruce on league page

--- resources/views/competitions/league.blade.php
+++ resources/views/competitions/league.blade.php
@@ -49,9 +49,19 @@
       @if ( $comp->hasResults() )
       <a href="{{ $comp->getResults()['link'] }}" target="_blank" class="font-semibold text-gray-600 hover:text-black flex mt-auto ">
         View Results
+
+        @if (array_key_exists('type', $comp->getResults()))
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6 ml-3">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+        </svg>
+
+        @else
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-3" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
           <path stroke-linecap="round" stroke-linejoin="round" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10" />
         </svg>
+        @endif
+
+
       </a>
       @else
       <p href="#" class="font-semibold text-red-600  flex mt-auto ">
