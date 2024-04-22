882e097e87b015b8c56e8f15bc90b07a8823a508
Noah Hollowell <noahdhollowell@googlemail.com>
2024-04-22 17:38:32 +0100

    g

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
     endurance, while helpful, do not necessarily make a good lifesaver!
 @endsection
 
-<div class=" min-h-[35vh] w-screen bg-gray-100  overflow-hidden  ">
+<div class=" h-[35vh] w-screen bg-gray-100  overflow-hidden  ">
 
 
 
