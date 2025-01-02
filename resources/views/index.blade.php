c9776848717c68d4779686300718521a90f0721f
Noah Hollowell <noahdhollowell@gmail.com>
2025-01-02 16:09:34 +0000

    Add champs hero

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -27,6 +27,22 @@
                 </div>
             
                 <div id="panes" class="h-full w-full flex flex-row overflow-hidden overflow-x-auto snap-x snap-mandatory thin-scrollbar ">
+
+
+                    <div class="min-w-full snap-center flex flex-col items-center justify-center" style="background: #004490">
+                        <img src="{{ asset('storage/photos/champs/2025/champs-logo-orange.svg') }}" class=" w-56" alt="" srcset="">
+                        <div class="flex flex-col items-center">
+                            <p class="text-white 2xl:text-4xl text-3xl font-bold uppercase text-center md:text-left">15-16th March</p>
+                            <p class="text-gray-300 2xl:text-xl text-lg font-semibold uppercase text-center md:text-left">@ K2 Crawley</p>
+                        </div>
+                        
+                        <a href="{{ route('champs.2025') }}" rel="noopener noreferrer"
+                            class="btn btn-thinner mt-6 !text-white !bg-[#004490] hover:!bg-[#f48c00] hover:!border-[#f48c00]">
+                            Find out More
+                        </a>
+                    </div>
+
+
                     <div class=" min-w-full snap-center head-bg-3 flex flex-col items-center justify-center transition-opacity   duration-1000 !bg-right md:bg-center  "
                         style="background-image: url('storage/photos/freshers/freshers (4).jpeg')" id="head1">
                         
@@ -65,12 +81,38 @@
             const inactiveClasses = ['bg-gray-100', 'opacity-90']
             const baseClasses = ['cursor-pointer', 'size-3', 'rounded-full']
 
+            const max_index = panes.children.length
+            let current_index = 0
             
+            const switchToPane = (i) => {
+                const child = panes.children[i]
+                current_index = i
+
+
+                panes.scrollTo({
+                    left: i * child.clientWidth,
+                    behavior: 'smooth'
+                })
+
+                //child.scrollIntoView({behavior: 'smooth', block:'nearest'})
+
+                for (var j = 0; j < panes.children.length; j++) {
+                    var jumper = controls.children[j]
+
+                    jumper.classList.remove(...activeClasses, ...inactiveClasses)
+                    console.log(i,j)
+                    if (j == i) {
+                        jumper.classList.add(...activeClasses)
+                    } else {
+                        jumper.classList.add(...inactiveClasses)
+                    }
+                }
+            }
+
 
             let first = true
 
             for (let i = 0; i < panes.children.length; i++) {
-                let child = panes.children[i]
                 jumper = document.createElement('div')
                 jumper.classList.add(...baseClasses)
 
@@ -82,24 +124,25 @@
                 }
 
                 jumper.onclick = (e) => {
-                    child.scrollIntoView({behavior: 'smooth'})
-
-                    for (var j = 0; j < panes.children.length; j++) {
-                        var jumper = controls.children[j]
-
-                        jumper.classList.remove(...activeClasses, ...inactiveClasses)
-                        console.log(i,j)
-                        if (j == i) {
-                            jumper.classList.add(...activeClasses)
-                        } else {
-                            jumper.classList.add(...inactiveClasses)
-                        }
-                    }
+                    switchToPane(i)
 
                 }
 
                 controls.appendChild(jumper)
             }
+
+            setInterval(() => {
+                current_index++
+
+                if (current_index >= max_index) {
+                    current_index = 0
+                }
+
+                switchToPane(current_index)
+
+                
+
+            }, 7000);
 
         </script>    
     
@@ -113,56 +156,68 @@
 
 
 <div class=" container-responsive ">
-    <div class=" grid xl:grid-cols-3 grid-cols-1 overflow-hidden md:gap-32 text-center">
-        <div class="flex flex-col items-center space-y-3 w-full">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24"
+    <div class="flex flex-col space-y-9 ">
+        <div class="flex flex-col md:flex-row items-center justify-center md:space-x-12 space-y-6 md:space-y-0 text-center md:text-left w-full">
+            
+            <svg xmlns="http://www.w3.org/2000/svg" class=" h-16 w-16" fill="none" viewBox="0 0 24 24"
                 stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round"
                     d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
             </svg>
-            <h2 class="text-2xl font-semibold ">Acting Governing Body</h2>
-            <p class="">
-                BULSCA acts as the governing body for lifesaving sport while at University. We are an organisation
-                overseeing the development of university lifesaving clubs, and ensure the smooth-running of an annual
-                league.
-            </p>
+            <div class="flex flex-col max-w-3xl ">
+                <h2 class="text-2xl font-semibold ">Acting Governing Body</h2>
+                
+                <p class="">
+                    BULSCA acts as the governing body for lifesaving sport while at University. We are an organisation
+                    overseeing the development of university lifesaving clubs, and ensure the smooth-running of an annual
+                    league.
+                </p>
+            </div>
         </div>
-        <div class="flex flex-col items-center space-y-3">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24"
+
+        <div class="flex flex-col md:flex-row items-center justify-center md:space-x-12 space-y-6 md:space-y-0 text-center md:text-left w-full">
+            
+            
+            <div class="flex flex-col max-w-3xl ">
+                <h2 class="text-2xl font-semibold ">By Students, For Students</h2>
+                <p>
+                    The BULSCA committee is made up entirely of volunteers, either current students or graduates from higher
+                    education institutions.
+                </p>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" class=" h-16 w-16" fill="none" viewBox="0 0 24 24"
                 stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round"
                     d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
             </svg>
-            <h2 class="text-2xl font-semibold ">By Students, For Students</h2>
-            <p>
-                The BULSCA committee is made up entirely of volunteers, either current students or graduates from higher
-                education institutions.
-            </p>
         </div>
-        <div class="flex flex-col items-center space-y-3">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24"
+
+        <div class="flex flex-col md:flex-row items-center justify-center md:space-x-12 space-y-6 md:space-y-0 text-center md:text-left w-full">
+            
+            <svg xmlns="http://www.w3.org/2000/svg" class=" h-16 w-16" fill="none" viewBox="0 0 24 24"
                 stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
             </svg>
+            <div class="flex flex-col max-w-3xl ">
             <h2 class="text-2xl font-semibold ">Our aims</h2>
             <p>
                 Our aims are to promote the training and development of lifesaving skills, to promote and develop
                 lifesaving as a sport and to oversee competitive lifesaving between Higher Education institutions.
             </p>
+            </div>
         </div>
     </div>
 </div>
-<div class="container-boast">
-    <p class="text-white 2xl:text-5xl text-4xl font-bold uppercase text-center md:text-left">We're on Facebook, go have
-        a look!</p>
-    <a href="https://www.facebook.com/BULSCA/" rel="noopener noreferrer"
-        class="btn md:ml-auto flex flex-row items-center mt-8 md:mt-0">
-        To Facebook
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-3" fill="none" viewBox="0 0 24 24"
-            stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round"
-                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
-        </svg>
+<div class="container-boast" style="background: #004490">
+    <img src="{{ asset('storage/photos/champs/2025/champs-logo-orange.svg') }}" class=" w-56" alt="" srcset="">
+    <div class="flex flex-col">
+        <p class="text-white 2xl:text-4xl text-3xl font-bold uppercase text-center md:text-left">15-16th March</p>
+        <p class="text-gray-300 2xl:text-xl text-lg font-semibold uppercase text-center md:text-left">@ K2 Crawley</p>
+    </div>
+    
+    <a href="{{ route('champs.2025') }}" rel="noopener noreferrer"
+        class="btn btn-thinner md:ml-auto mt-6 md:mt-0">
+        Find out More
     </a>
 </div>
 
