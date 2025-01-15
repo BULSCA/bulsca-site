a147c0ecdb36b48d75615d0ceb7c56dfd9a0464f
Noah Hollowell <noahdhollowell@googlemail.com>
2025-01-15 11:59:11 +0000

    add base for image deletion

--- resources/views/admin/sercs/casualties/show.blade.php
+++ resources/views/admin/sercs/casualties/show.blade.php
@@ -45,15 +45,21 @@
                     handleImages() {
                         const files = this.$refs.images.files;
                         for (let i = 0; i < files.length; i++) {
+                
+                            let id = Math.random().toString(36).substring(7)
+                
                             this.images.push({
+                                id: id,
                                 file: files[i],
                                 preview: URL.createObjectURL(files[i]),
                                 state: 'uploading',
                             });
                 
+                            let image = this.images.find(image => image.id === id)
+                
                 
                             let fd = new FormData()
-                            fd.append('image', files[i])
+                            fd.append('image', image.file)
                 
                 
                             fetch('{{ route('admin.sercs.casualties.images.add', $casualty) }}', {
@@ -69,11 +75,16 @@
                 
                                 showNotification('Uploaded and image')
                 
-                                this.images[i].state = 'uploaded'
-                                this.images[i].preview = data.url
+                                image.state = 'uploaded'
+                                image.preview = data.url
                             })
                         }
                     },
+                
+                    deleteImage(image) {
+                        let index = this.images.indexOf(image)
+                        this.images.splice(index, 1)
+                    }
                 
                 
                 
@@ -110,7 +121,7 @@
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                             stroke-width="1.5" stroke="currentColor"
                                             class="w-6 h-6 text-white opacity-0 group-hover:opacity-100 cursor-pointer"
-                                            @click="removeInput(index)">
+                                            @click="deleteImage(image)">
                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                 d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                         </svg>
