3898a259cd63c172da06a3286d1057f8353cb806
jo3e22 <jo3e22@soton.ac.uk>
2025-11-04 18:50:54 +0000

    test of custom factory for image service

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -4,18 +4,18 @@ namespace App\Services;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use League\Glide\Responses\LaravelResponseFactory;
 use League\Glide\ServerFactory;
+use League\Glide\Responses\ResponseFactoryInterface;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ImageService
 {
     public static function server()
     {
-
         $driver = Storage::disk('local')->path('');
 
         $server = ServerFactory::create([
-            'response' => new LaravelResponseFactory(app('request')),
+            'response' => new LaravelGlideResponseFactory(),
             'source' => $driver.'img/source',
             'cache' => $driver.'img/cache',
             'cache_path_prefix' => '.cache',
@@ -27,13 +27,11 @@ class ImageService
 
     public static function get($path)
     {
-
         return self::server()->getImageResponse($path, request()->all());
     }
 
     public static function getUrl($path)
     {
-
         return route('image', ['path' => $path]);
     }
 
@@ -52,5 +50,28 @@ class ImageService
     {
         Storage::delete('img/source/'.$path);
         Storage::delete('img/cache/'.$path);
+    }
+}
+
+class LaravelGlideResponseFactory implements ResponseFactoryInterface
+{
+    public function create($cache, $content)
+    {
+        $response = new StreamedResponse();
+        $response->headers->set('Content-Type', $cache->getMimeType());
+        $response->headers->set('Content-Length', $cache->getSize());
+        $response->headers->set('Cache-Control', 'max-age=31536000, public');
+        $response->headers->set('Expires', gmdate('D, d M Y H:i:s \G\M\T', time() + 31536000));
+        
+        $stream = $cache->getStream();
+        $response->setCallback(function () use ($stream) {
+            if (ftell($stream) !== 0) {
+                rewind($stream);
+            }
+            fpassthru($stream);
+            fclose($stream);
+        });
+
+        return $response;
     }
 }
