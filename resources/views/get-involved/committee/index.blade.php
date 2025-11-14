e74373a39dfeaca2e955d503a65fb7614772a2d6
jo3e22 <jo3e22@soton.ac.uk>
2025-11-14 17:13:41 +0000

    Refactor committee member card layout for improved structure and consistency

--- resources/views/get-involved/committee/index.blade.php
+++ resources/views/get-involved/committee/index.blade.php
@@ -32,18 +32,16 @@
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-3 md:gap-y-9 pb-8 justify-items-center">
 
             @foreach ($members as $member)
-                <a href="{{ route('committee.member.view', ['cid' => $committee->date_slug, 'nameslug' => $member->nameSlug]) }}" class="no-underline text-black">
-                    <div class="flex flex-col justify-between items-center rounded-md border hover:border-bulsca transition no-underline text-center overflow-hidden min-h-80 w-56">
-                        <div class="rounded-full w-44 h-44 overflow-hidden flex items-center justify-center mt-4 mx-4">
-                            <img src="{{ $member->image_path ? route('image', $member->image_path) : '/storage/logo/blogo.png' }}" class="w-full h-full" alt="">
-                        </div>
-                        <h3 class="header header-smallish px-4">
-                            {{ $member->name }}
-                        </h3>
+                <a href="{{ route('committee.member.view', ['cid' => $committee->date_slug, 'nameslug' => $member->nameSlug]) }}" class="flex flex-col justify-between items-center rounded-md border hover:border-bulsca transition no-underline text-center overflow-hidden min-h-80 w-56">
+                    <div class="rounded-full w-44 h-44 overflow-hidden flex items-center justify-center mt-4 mx-4">
+                        <img src="{{ $member->image_path ? route('image', $member->image_path) : '/storage/logo/blogo.png' }}" class="w-full h-full" alt="">
+                    </div>
+                    <h3 class="header header-smallish px-4">
+                        {{ $member->name }}
+                    </h3>
 
-                        <div class="bg-bulsca w-full font-semibold text-white p-2 rounded-b text-center">
-                            {{ $member->role->label }}
-                        </div>
+                    <div class="bg-bulsca w-full font-semibold text-white p-2 rounded-b text-center">
+                        {{ $member->role->label }}
                     </div>
                 </a>
             @endforeach
