e80ddfbdd07bafbdce723913f2538f8f39b2f464
jo3e22 <jo3e22@soton.ac.uk>
2025-12-13 17:50:02 +0000

    Refactor legacy login view for improved clarity and structure

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,60 +1,54 @@
 @extends('layout')
 
 @section('title')
-Login |
+Legacy Login |
 @endsection
 
 @section('content')
-
 <x-page-banner
-    title="Login"
+    title="Legacy Login"
     height='h-[30vh]'
     :snowContainer="true"
 />
 
-
 <div class="container-responsive">
-  <div class="flex flex-col items-center justify-center">
+    <div class="flex flex-col items-center justify-center">
+        @if($errors->any())
+        <div class="text-bulsca_red font-semibold my-3">
+            {!! implode('<br />', $errors->all('<span>:message</span>')) !!}
+        </div>
+        @endif
 
-    @if($errors->any())
-    <div class=" text-bulsca_red font-semibold my-3">
+        <form action="{{ route('login.legacy') }}" class="flex flex-col w-[80%] lg:w-[50%] 2xl:w-1/3" method="post">
+            @csrf
+            
+            <div class="form-input">
+                <label for="login-email">Email</label>
+                <input id="login-email" class="input" name="email" required type="text" value="{{old('email')}}" @if(!old('email')) autofocus @endif placeholder="official@bulsca.co.uk">
+            </div>
+            
+            <div class="form-input">
+                <label for="login-password">Password</label>
+                <input id="login-password" class="input" name="password" @if(old('email')) autofocus @endif required type="password">
+            </div>
+            
+            <div class="form-checkbox">
+                <input id="login-remember" name="remember" type="checkbox">
+                <label for="login-remember">Remember Me</label>
+            </div>
+            
+            <a href="{{ route('password.request') }}"><small>Forgot Password?</small></a>
+            
+            <button class="ml-auto btn btn-thinner">
+                Login
+            </button>
+        </form>
 
-
-      {!! implode('<br />', $errors->all('<span>:message</span>')) !!}
-
+        <div class="text-center mt-6">
+            <a href="{{ route('login') }}" class="text-sm text-gray-600 hover:text-bulsca_red transition">
+                ← Back to login options
+            </a>
+        </div>
     </div>
-    @endif
-
-    <form action="{{ route('login') }}" class="flex flex-col w-[80%] lg:w-[50%] 2xl:w-1/3" method="post">
-      @csrf
-      <div class="form-input">
-        <label for="login-email">Email</label>
-        <input id="login-email" class="input" name="email" required type="text" value="{{old('email')}}" @if(!old('email')) autofocus @endif placeholder="official@bulsca.co.uk">
-      </div>
-      <div class="form-input">
-        <label for="login-password">Password</label>
-        <input id="login-password" class="input" name="password" @if(old('email')) autofocus @endif required type="password">
-      </div>
-      <div class="form-checkbox">
-        <input id="login-remember" name="remember" type="checkbox">
-        <label for="login-remember">Remember Me</label>
-      </div>
-
-      <a href="{{ route('password.request') }}"><small>Forgot Password?</small></a>
-
-      <button class="ml-auto btn btn-thinner">
-        Login
-      </button>
-    </form>
-
-    <div class="text-center mt-4">
-        <a href="{{ route('login') }}" class="text-sm text-gray-600 hover:text-gray-900">
-            ← {{ __('Back to login options') }}
-        </a>
-    </div>
-
-  </div>
 </div>
-
-
 @endsection
