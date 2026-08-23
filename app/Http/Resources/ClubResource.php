9f9e97a9dc08ddda4507e6e498447659afa057f1
jo3e22 <jo3e22@soton.ac.uk>
2026-08-23 09:26:10 +0200

    Add ClubResource for API response formatting and update test HTTP requests

---
+++ app/Http/Resources/ClubResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+
+class ClubResource extends BaseResource
+{
+    public function toArray(Request $request): array
+    {
+        $data = [
+            'id' => $this->id,
+            'slug' => $this->slug,
+            'title' => $this->title,
+            'author' => $this->author,
+            'pinned' => $this->pinned,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'views' => $this->views,
+            'thumbs_up' => $this->thumbs_up,
+            'thumbs_down' => $this->thumbs_down
+        ];
+
+        return $data;
+    }
+}
